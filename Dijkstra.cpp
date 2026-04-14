b3c6b50e0248213ed7b499898d1fdb6cc01aa10a
pravadaR <pravada290407@gmail.com>
2026-04-14 11:26:40 +0530

    Fix variable naming and logic in Dijkstra's algorithm

--- Dijkstra.cpp
+++ Dijkstra.cpp
@@ -19,9 +19,60 @@ void dijkstra(int src)  //declares a function called dijkstra that takes the sou
     }
 dist[src]=0;   //distance from source to itself is always 0
   for(int count=0; count<V-1;count++)
-int U =-1;    //start with no vertex selected
+int u=-1;    //start with no vertex selected
 for(int i=0; i<V;i++) //loop through all vertices to find the unvisited one with the smallest distance
 {
-  if(!visited[i] && (U==-1 || dist[i]<dist[U]))  //only consider unvisited vertices 
+  if(!visited[i] && (u==-1 || dist[i]<dist[U]))  //only consider unvisited vertices 
 //if no vertex pickes yet , pick this one or if this vertex has a shortest distance pick it instead
 //after the loop, U holds the closest unvisited vertex
+    u=i;
+}
+if(dist[u]==INF) break;
+visited[u]=true;
+for(int v=0; v<V;v++)
+{
+    if(!visited[v] && graph[u][v]  != 0)
+  {
+      int newDsit=dist[u] + graph[u][v];
+      if(newDist<dist[v])
+        dist[v]=newDist;
+      }
+    }
+  }
+}
+int main()
+{
+  cout<<"Enter number of vertices: ";
+  cin>>V;
+  cout<<"Enter number of edges: ";
+  cin>>E;
+
+  for(int i=0; i<V;i++)
+    for(int j=0; i<V;j++)
+      graph[i][j]=0;
+
+  cout<<"ENter edges (u v weight)- vertices are 0-indexes:\n";
+for(int i=0; i<E; i++)
+{
+  int u,v,w;
+  cin>>u>>v>>w;
+  graph[u][v]=w;
+  graph[v][u]=w;
+}
+int src;
+cout<<"Enter source vertex: ";
+cin>>src;
+dijkstra(src);
+
+cout<<"\nShortest distance from vertex"<<src<<":\n";
+for(int i=0;i<V;i++)
+{
+  cout<<"  To vertex "<<i<<": ";
+  if(dist[i]==INF)
+    cout<<"unreachable\n";
+else
+    cout<<"dist[i]<<"\n";
+}
+return 0;
+}
+
